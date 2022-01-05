a1e983ce180dcb40bd68936e1517aa7dde953155
ddijk <dick@dijkrosoft.nl>
2022-01-05 09:27:57 +0100

    working

--- src/main/java/nl/dijkrosoft/bgk_overzicht/RunnerBgkNativeSQL.java
+++ src/main/java/nl/dijkrosoft/bgk_overzicht/RunnerBgkNativeSQL.java
@@ -1,8 +1,9 @@
 package nl.dijkrosoft.bgk_overzicht;
 
-import nl.bytesoflife.clienten.hoofdsom.HoofdsomInvoer;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 public class RunnerBgkNativeSQL {
